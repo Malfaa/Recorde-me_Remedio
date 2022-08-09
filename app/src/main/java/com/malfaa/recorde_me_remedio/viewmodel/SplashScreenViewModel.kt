264e38116166e8821ca2a5dba16d1df61d56b517
gustavo <7110999@gmail.com>
2022-08-09 11:52:39 -0300

    refactor: Refazendo código base
    Decidi refatorar o código por conta da forma em que foi feito o código, após o curso, novas/antigas técnicas foram aperfeiçodas. Logo, refatorar por completo foi a melhor decisão.

--- app/src/main/java/com/malfaa/recorde_me_remedio/viewmodel/SplashScreenViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.malfaa.recorde_me_remedio.viewmodel
-
-import android.annotation.SuppressLint
-import androidx.lifecycle.ViewModel
-import com.malfaa.recorde_me_remedio.room.LDao
-
-@SuppressLint("CustomSplashScreen")
-class SplashScreenViewModel(private val dao: LDao)  : ViewModel() {
-}
