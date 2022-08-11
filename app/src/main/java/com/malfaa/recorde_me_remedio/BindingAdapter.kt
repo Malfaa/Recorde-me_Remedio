4051795a2c17abd1871a089c2a5b58933a9c3367
gustavo <7110999@gmail.com>
2022-08-10 21:16:20 -0300

    refactor: 'Clicks' está com o tipo devido e terminaValor(item_remedio.kt) foi corrigido.

--- app/src/main/java/com/malfaa/recorde_me_remedio/BindingAdapter.kt
+++ app/src/main/java/com/malfaa/recorde_me_remedio/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.malfaa.recorde_me_remedio
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.util.Log
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.databinding.ObservableField
 import java.time.LocalDate
 import java.util.*
 
+@SuppressLint("SetTextI18n")
 @BindingAdapter("conversorPosEmData", "verificaData", requireAll = true)
 fun conversorPosEmData(textView : TextView, data: Int, verificaData: Boolean) {
     if(!verificaData){
@@ -25,6 +27,7 @@ fun conversorPosEmData(textView : TextView, data: Int, verificaData: Boolean) {
     }
 }
 
+@SuppressLint("SetTextI18n")
 @BindingAdapter("conversorPosEmHoras", "verificaHora", requireAll = true)
 fun conversorPosEmHoras(textView : TextView, valor: Int, verifica: Boolean) {
     if(!verifica){
@@ -41,58 +44,22 @@ fun conversorPosEmHoras(textView : TextView, valor: Int, verifica: Boolean) {
     }
 }
 
-@BindingAdapter("calendario", "verificaCalendario", requireAll = true)
-fun calendario(textView : TextView, item: Int, verifica: Boolean){
-    if (!verifica) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val diaAtual = LocalDate.now()
-            Log.d("28+", "SDK superior")
-            when (item) {
-                1/*"5 dias"*/ -> stringFormat(diaAtual.plusDays(5).dayOfMonth)+ "/" + stringFormat(diaAtual.plusDays(5).monthValue)
-                2/*"7 dias"*/ -> stringFormat(diaAtual.plusDays(7).dayOfMonth)+ "/" + stringFormat(diaAtual.plusDays(7).monthValue)
-                3/*"14 dias"*/-> stringFormat(diaAtual.plusDays(14).dayOfMonth)+ "/" + stringFormat(diaAtual.plusDays(14).monthValue)
-                //"Todos os dias" -> diaAtual.plusDays(5) //talvez criar alguma var que altere um fun que daí escreve os diaAtual
-            }
-        }
-        else {
-            Log.d("28-", "SDK inferior")
-            val calendario = Calendar.getInstance()
-
-            val valorSelecionado: Int =
-                when (item) {
-                    1 -> 5
-                    2 -> 7
-                    3 -> 14
-                    else -> 0
-                }
-//------------------------------------  -------------------
-            calendario.add(Calendar.DATE, valorSelecionado)
-            calendarioParaData(calendario.time)
-        }
-    } else {
-        val calendario = Calendar.getInstance()
-
-        calendario.add(Calendar.DATE, item)
-        textView.text = calendarioParaData(calendario.time)
-    }
-}
-
 @BindingAdapter("notaValue")
-fun notaView(view:View, nota:String){
+fun notaValue(view:View, nota:String){
     if (nota.isEmpty()){
         view.visibility = View.GONE
     }
 }
 
 @BindingAdapter("clicks")
-fun listenClicks(spinner: AppCompatSpinner, result: ObservableField<Int>) {
+fun listenClicks(spinner: AppCompatSpinner, result: Int) {// FIXME: erro aqui
     spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
         override fun onNothingSelected(parent: AdapterView<*>?) {
 
         }
 
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            result.set(parent?.getItemAtPosition(position) as Int)
+            parent?.getItemAtPosition(result) as Int
         }
     }
 }
