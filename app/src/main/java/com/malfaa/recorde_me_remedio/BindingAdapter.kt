e852bbb86f54f07a685297963587862ca69052d0
gustavo <7110999@gmail.com>
2022-10-20 19:42:21 -0300

    feat & refactor: Alteração do ícone launcher e demais layouts
    Pequenas alterações nos layouts, adição ao BindingAdapter.kt, novas cores, novos ícones
    
    Resolução: Sem resoulução

--- app/src/main/java/com/malfaa/recorde_me_remedio/BindingAdapter.kt
+++ app/src/main/java/com/malfaa/recorde_me_remedio/BindingAdapter.kt
@@ -1,6 +1,10 @@
 package com.malfaa.recorde_me_remedio
 
+import android.annotation.SuppressLint
 import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("notaValue")
@@ -8,4 +12,15 @@ fun notaValue(view:View, nota:String?){
     if (nota.isNullOrEmpty()){
         view.visibility = View.GONE
     }
+}
+
+@BindingAdapter("mudaExemplo")
+fun mudaExemplo(editText: EditText, textView: TextView){
+    textView.text = String.format("%s em %s hora(s)", editText.text)
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("todososdias")
+fun todososdias(textView: TextView, item: Boolean){
+    if(item) textView.text = "Todos os Dias"
 }
